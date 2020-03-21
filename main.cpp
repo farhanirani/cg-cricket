8fbbd8b3dbedbbafc9783bf134e5eea0a0f7575d
unknown <farhanirani007@gmail.com>
2020-03-21 23:21:17 +0530

    better

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@
 #include<math.h>
 #include<unistd.h>
 
-float x,y,xc=1050,yc=900,r=813, bowlerhandx, bowlerhandy = 420;
+float x,y,xc=1050,yc=870,r=813, bowlerhandx, bowlerhandy = 420;
 
 void initial()  //for inital runup and bat swing
 {
 	cleardevice();
 	
 	//batsman
-	x=400;
+	x=220;
 	setfillstyle(SOLID_FILL,BLUE);
 	setcolor(WHITE);
-	circle(x-60,450,18);
-	floodfill(x-60,450,WHITE);
+	circle(x,450,18);
+	floodfill(x,450,WHITE);
 	
 	//bowler
 	x=1100;
 	bowlerhandx = x+40;
+	//runup
 	while(x>900)
 	{
 		setfillstyle(SOLID_FILL,GREEN);
@@ -61,19 +62,61 @@ void initial()  //for inital runup and bat swing
 		bowlerhandx-=2.8;
 	}
 	
-	//bowler last pos
+	//bowlers last pos
 	setfillstyle(SOLID_FILL,GREEN);
 	setcolor(WHITE);
 	circle(x,450,18);
 	floodfill(x,450,WHITE);
     line(x,468,x,535);
-	line(x,500,bowlerhandx,400);
+	line(x,500,bowlerhandx,410);
 	line(x,500,x+20,540);
 	line(x,535,x+15,575);
 	line(x,535,x-15,575);
-
 	
-	x=500;
+	x=bowlerhandx;
+	//ball throw   .................................
+	int xc1 = 873, yc1 = 723, rc1 = 323;
+	while(x>600)
+	{
+		y = yc1 - sqrt(rc1*rc1 - pow((x-xc1),2));
+		
+		setfillstyle(SOLID_FILL,RED);
+		setcolor(WHITE);
+		circle(x,y,10);
+		floodfill(x,y,WHITE);
+		
+		delay(10);
+		
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(x,y,WHITE);
+		setcolor(BLACK);
+		circle(x,y,10);
+		
+		x-=3;
+	}
+	xc1 = 431; yc1 = 640; rc1 = 190;
+	while(x>350)
+	{
+		y = yc1 - sqrt(rc1*rc1 - pow((x-xc1),2));
+		
+		setfillstyle(SOLID_FILL,RED);
+		setcolor(WHITE);
+		circle(x,y,10);
+		floodfill(x,y,WHITE);
+		
+		delay(10);
+		
+		setfillstyle(SOLID_FILL,BLACK);
+		floodfill(x,y,WHITE);
+		setcolor(BLACK);
+		circle(x,y,10);
+		
+		x-=3;
+	}
+	
+	//hits bat..................................
+	
+	
 }
 
 void six()  //for ball to go for a six
